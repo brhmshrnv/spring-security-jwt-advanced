e03d218ac34cf2554e301d63569bd5cd39caa3b2
Ibrahim Shirinov <ibrahim.shirinovv@gmail.com>
2021-09-07 15:08:38 +0400

    added security constants and jwt library

---
+++ src/main/java/az/ibrahimshirinov/springsecurityjwtadvanced/constant/SecurityConstant.java
@@ -0,0 +1,21 @@
+package az.ibrahimshirinov.springsecurityjwtadvanced.constant;
+
+/**
+ * @author IbrahimShirinov
+ * @since 07.09.2021
+ */
+public class SecurityConstant {
+
+    public static final long EXPIRATION_TIME = 432_000_000;// 5 days expressed in milliseconds
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String TOKEN_ISSUE = "ibrahimshirinov.com";
+    public static final String ABOUT_PROJECT = "User Management Portal";
+    public static final String AUTHORITIES = "Authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = {"/user/login","/user/register/","/user/resetpassword/**","/user/image/**"};
+
+}
