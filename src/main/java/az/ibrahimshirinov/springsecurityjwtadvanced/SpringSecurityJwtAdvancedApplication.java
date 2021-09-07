daf1993a971db826dc5ca67c1c0c99680fa1b121
Ibrahim Shirinov <ibrahim.shirinovv@gmail.com>
2021-09-07 11:55:03 +0400

    project initialized

---
+++ src/main/java/az/ibrahimshirinov/springsecurityjwtadvanced/SpringSecurityJwtAdvancedApplication.java
@@ -0,0 +1,13 @@
+package az.ibrahimshirinov.springsecurityjwtadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ibrahimshirinov/springsecurityjwtadvanced/SpringSecurityJwtAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ibrahimshirinov.springsecurityjwtadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
